cf26adf3ca667228adad6469089c34a21d812e60
ayshahide <150399667+ayshahide@users.noreply.github.com>
2026-02-17 21:15:22 +0000

    Write unit tests for displaying ratings in expanded deal or on deal card

--- client/src/components/Deals/ExpandedDeal.test.js
+++ client/src/components/Deals/ExpandedDeal.test.js
@@ -20,6 +20,20 @@ describe('ExpandedDeal', () => {
         dealEditData: '2024-01-01 13:16',
     };
 
+    const mockDeal2 = {
+        dealID: 1,
+        dealName: 'Burger Special',
+        restaurantID: 123,
+        restaurantName: 'Test Restaurant',
+        dealDescription: 'Tasty burger with fries',
+        dealPrice: 9.99,
+        numRatings: 0,
+        dealValueRating: 0,
+        dealTasteRating: 0,
+        dealPortionRating: 0,
+        dealEditData: '2024-01-01 13:16',
+    };
+
     const mockHours = [
         {
         dayOfWeek: 'Monday',
@@ -100,8 +114,7 @@ describe('ExpandedDeal', () => {
 
     });
 
-
-    test('shows loading message while hours are loading', async () => {
+    test('shows loading message while hours are loading in expanded deal info', async () => {
         let resolveFetch;
         global.fetch = jest.fn(() => new Promise((res) => { resolveFetch = res }));
 
@@ -111,10 +124,47 @@ describe('ExpandedDeal', () => {
         expect(screen.getByText(/Loading availability.../)).toBeInTheDocument();
     });
 
-    test('shows error message if hours fail to load', () => {
+    test('shows error message if hours fail to load in expanded deal info', () => {
         global.fetch = jest.fn(() => {throw new Error('API failure')});
         renderComponent();
         expect(screen.getByText(/Failed to load availability/)).toBeInTheDocument();
     
     });
+
+    test('deals with no ratings display "No ratings yet" in expanded deal info', () => {
+        handleClose = jest.fn().mockName('handleClose');
+        render(<ExpandedDeal
+            deal={mockDeal2}
+            open={true}
+            uuid={null}
+            handleClose={handleClose}
+            />);
+
+        expect(screen.getByText(/No ratings yet/i)).toBeInTheDocument();
+    });
+
+    test('ratings are displayed consistently with a star and (one decimal point)/5 in expanded deal info', () => {
+        renderComponent();
+        const avg40 = screen.getAllByText((/⭐ 4\.0\/5/i));
+        expect(avg40).toHaveLength(2);
+        expect(screen.getByText((/⭐ 5\.0\/5/i)))
+        expect(screen.getByText((/⭐ 3\.0\/5/i)))
+        expect(/Overall Rating/i).toBeInTheDocument()
+        expect(/Portion Size Rating/i).toBeInTheDocument()
+        expect(/Taste Rating/i).toBeInTheDocument()
+        expect(/Value Rating/i).toBeInTheDocument()
+        
+    })
+
+    test('average ratings are correctly calculated in expanded deal info', () => {
+        renderComponent();
+        const average = ((mockDeal.dealTasteRating + mockDeal.dealValueRating + mockDeal.dealPortionRating) / 3).toFixed(1)
+        expect(screen.getByText(new RegExp(`⭐ ${average}/5`))).toBeInTheDocument()
+    })
+
+    test('the number of ratings contributing to the score can be seen in expanded deal info', () => {
+        renderComponent();
+        expect(screen.getByText(/(10 Ratings)/i)).toBeInTheDocument();
+    })
+    
 });
