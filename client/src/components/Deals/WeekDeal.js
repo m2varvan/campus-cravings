a8107c76d6781bcd5ddae39f5b60914a5cc0d73e
ayshahide <150399667+ayshahide@users.noreply.github.com>
2026-02-18 20:17:15 +0000

    Client side API for loading week deals

--- client/src/components/Deals/WeekDeal.js
+++ client/src/components/Deals/WeekDeal.js
@@ -1,12 +1,35 @@
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Box, CircularProgress } from "@mui/material";
+import React from "react";
 
-const WeekDeal = ({uuid}) => {
+const WeekDeal = ({uuid, weekDeals, loadWeekDeals, loading, error}) => {
+
+    React.useEffect(() => {
+        loadWeekDeals();
+    }, [])
 
     return(
         <Grid item xs={12}>
             <Typography variant='h5' sx={{ my: 2 }}>
                 Weekly Deals (Monday to Sunday)
             </Typography>
+
+            {/* Show loading message when deals are loading */}
+            {loading && (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',   
+                        justifyContent: 'center',
+                        width: '100%',      
+                        textAlign: 'center',
+                        gap: 2,
+                    }}
+                >
+                    <CircularProgress size={48} color="secondary" />
+                    <Typography variant={'h6'}>Loading Weekly Deals</Typography>
+                </Box>
+            )}
         </Grid>
     )
 };
