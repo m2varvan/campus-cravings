da3c69936ff466e055f76d06aee0e8381b6f84dc
ayshahide <150399667+ayshahide@users.noreply.github.com>
2026-02-19 00:32:16 +0000

    Log in to rate deal message

--- client/src/components/Deals/RateDeal.js
+++ client/src/components/Deals/RateDeal.js
@@ -6,24 +6,31 @@ const RateDeal = ({uuid, deal}) => {
     return(
          <Box sx={{ width: '50%' }}>
             <Typography variant="subtitle1" fontWeight={600} gutterBottom>
-            My Ratings
+                My Ratings
             </Typography>
 
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography variant="body2">Value</Typography>
-            <Typography variant="body2">My Value Rating Here</Typography>
-            </Box>
+            { !uuid ?
+                <Typography>Log in to rate this deal.</Typography>
+            :
+            <>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="body2">Value</Typography>
+                    <Typography variant="body2">My Value Rating Here</Typography>
+                </Box>
 
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography variant="body2">Taste</Typography>
-            <Typography variant="body2">My Taste Rating Here</Typography>
-            </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="body2">Taste</Typography>
+                    <Typography variant="body2">My Taste Rating Here</Typography>
+                </Box>
 
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography variant="body2">Portion</Typography>
-            <Typography variant="body2">My Portion Rating Here</Typography>
-            </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="body2">Portion</Typography>
+                    <Typography variant="body2">My Portion Rating Here</Typography>
+                </Box>
+            </>
+            }
         </Box>
+
 
     );
 
