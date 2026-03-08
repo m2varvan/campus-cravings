8e8f0a8bd8cf5be5f2b17b126891a0dced82f3e1
ayshahide <150399667+ayshahide@users.noreply.github.com>
2026-03-08 23:09:10 +0000

    Add jest test to test deal worth-it voting

---
+++ client/src/components/Deals/DealVote.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DealVote = ({uuid, totalScore, deal_id}) => {
+
+    // API to add vote
+
+    // API to edit vote
+
+    return(
+        <p>Voting Here</p>
+    )
+}
+
+export default DealVote;
+
