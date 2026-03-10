1a823c4aa398f38b7fd4afc78021dabe89a93d7d
ayshahide <150399667+ayshahide@users.noreply.github.com>
2026-03-10 22:40:16 +0000

    Add outline of Favourite component

--- client/src/components/Deals/Favourite.js
+++ client/src/components/Deals/Favourite.js
@@ -1,10 +1,44 @@
+import { IconButton } from "@mui/material";
 import React from "react";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
-const Favourite = ({type, uuid, fave, saveFavourite, }) => {
+const Favourite = ({type, uuid, itemID, fave, saveFave, removeFave }) => {
 
-    return(
-        <p>Favourite Here</p>
-    )
+    const [isFavourited, setIsFavourited] = React.useState(fave);
+
+    React.useEffect(() => {
+        setIsFavourited(fave);
+    }, [fave]);
+
+    const handleFavourite = () => {
+
+        if (!uuid) return;
+
+        if (isFavourited) {
+            React.removeFavourite?.(type, uuid, itemID);
+            setIsFavourited(false);
+        } else {
+            React.saveFavourite?.(type, uuid, itemID);
+            setIsFavourited(true);
+        }
+    };
+
+    return (
+        <IconButton
+            onClick={handleFavourite}
+            disabled={!uuid}
+            data-testid={isFavourited ? "favourite-filled" : "favourite-empty"}
+            style={{
+                background: "none",
+                border: "none",
+                cursor: uuid ? "pointer" : "not-allowed",
+                fontSize: "20px"
+            }}
+        >
+            {isFavourited ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+        </IconButton>
+    );
 }
 
 export default Favourite;
